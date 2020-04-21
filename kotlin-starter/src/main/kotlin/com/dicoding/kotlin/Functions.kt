8065ebb5bb8904caaf38fee2fdaf4c969a6718ec
Qanturis <osefalfan.f@gmail.com>
2020-04-22 00:16:25 +0700

    Update Functions.kt

--- kotlin-starter/src/main/kotlin/com/dicoding/kotlin/Functions.kt
+++ kotlin-starter/src/main/kotlin/com/dicoding/kotlin/Functions.kt
@@ -3,6 +3,7 @@ package com.dicoding.kotlin
 /*Struktur fungsi pada kotlin
         fun functionName(param1: Type1, param2: Type2, ...): ReturnType {
         return result
+    }
 */
 
 fun bioData(nama: string, usia: int, jenisKelamin: char, alamat:string) string{
