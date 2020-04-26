0a4f78f18e2848a0e57ee0465120e8de306579c9
Qanturis <osefalfan.f@gmail.com>
2020-04-26 17:39:40 +0700

    Update 2-260420

--- kotlin-starter/src/main/kotlin/com/dicoding/kotlin/String Template.kt
+++ kotlin-starter/src/main/kotlin/com/dicoding/kotlin/String Template.kt
@@ -1,2 +1,25 @@
 package com.dicoding.kotlin
 
+//Concatenation (+)
+/*fun main(){
+    val name = 523
+    print("My name is " + name)
+
+}*/
+
+//String Template ($)
+/*
+fun main(){
+    val name = "Osef Alfan Fadhil"
+    val int = 47
+    println("Namanya $name, usianya $int tahun.")
+
+}*/
+
+//With expression
+/*
+fun main(){
+    val hour = 7
+    println("Office ${if (hour > 7) "Already close" else "is open"}")
+
+}*/
