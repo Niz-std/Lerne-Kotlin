46c1edc4a1f7bc8703dd76e6113a72eb819615e2
Qanturis <osefalfan.f@gmail.com>
2020-04-29 23:13:32 +0700

    Update 290920

---
+++ kotlin-starter/src/main/kotlin/com/dicoding/kotlin/Expression dan Statements.kt
@@ -0,0 +1,36 @@
+package com.dicoding.kotlin
+/*Expression pada Kotlin terdiri dari variabel, operator dan sesuatu yang mengevaluasi ke dalam nilai tunggal.*/
+/*Statements nilainya langsung di declare, jadi ga return value*/
+
+/* If adalah contoh dari expression karena dia mengembalikan value (ketika kondisinya terpenuhi)
+fun main(){
+    val openOffice = 7
+    val now = 8
+    val office = if (now > openOffice) "Office is already open" else "Office is closed"
+    println(office)
+}
+*/
+
+//express in express
+/*fun main(){
+    sum(1, 1*4)
+}
+fun sum(value1: Int, value2: Int) = value1 + value2
+}*/
+
+//Contoh Statements : Declare variable
+/*
+fun main()  {
+    val value1 = 10
+    val value2 = 20
+    sum(value1, value2)
+    println(sum())
+}
+
+fun sum(value1 : Int, value2 : Int) = value1 + value2 {
+    return@value2
+}*/
+
+/* Lol gimana coba return valuenya
+  PR Coba Return value dari fungsi SUM, hint: Modul functions yang return value
+ */
