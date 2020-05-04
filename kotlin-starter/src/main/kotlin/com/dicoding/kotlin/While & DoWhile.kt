b5061e071f3ab2c47810f983fbc310238eef6ae5
Qanturis <osefalfan.f@gmail.com>
2020-05-05 00:05:44 +0700

    Update 05052020

---
+++ kotlin-starter/src/main/kotlin/com/dicoding/kotlin/While & DoWhile.kt
@@ -0,0 +1,37 @@
+package com.dicoding.kotlin
+
+/*
+fun main() {
+    var counter = 1
+    while (counter <= 8){
+        println("Hellow World!")
+        counter++
+    }
+
+}
+*/
+
+/*
+fun main(){
+    var counter = 1
+    do {
+        println("Hellow Worlds!!")
+        counter++
+    } while (counter <=7)
+
+}
+*/
+/*Berbeda dengan While, Do While bersifat Exit Controlled Loop di mana proses perulangan akan langsung dijalankan
+di awal. Jika telah selesai, barulah kondisi yang diberikan akan dievaluasi.*/
+
+//Infinite loop (!! Jangan di RUN)
+/*
+fun main(){
+    var value = 'A'
+    do {
+        print(value)
+    } while (value <= 'Z')
+
+}*/
+/*While dan Do While sendiri tidak dapat digunakan untuk melakukan perulangan pada rentan angka. Untuk melakukannya kita
+ bisa menggunakan For Loop yang akan kita pelajari pada modul selanjutnya.*/
